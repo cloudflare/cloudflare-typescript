b9f1f101406a3edaa2165c13289466fa0c42607a
Musa Jundi <musa@cloudflare.com>
2026-04-15 14:37:40 -0500

    chore(ai-gateway): update generated types and methods

--- src/resources/ai-gateway/provider-configs.ts
+++ src/resources/ai-gateway/provider-configs.ts
@@ -1,6 +1,7 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 import { APIResource } from '../../resource';
+import { isRequestOptions } from '../../core';
 import * as Core from '../../core';
 import { V4PagePaginationArray, type V4PagePaginationArrayParams } from '../../pagination';
 
@@ -29,7 +30,7 @@ export class ProviderConfigs extends APIResource {
     params: ProviderConfigCreateParams,
     options?: Core.RequestOptions,
   ): Core.APIPromise<ProviderConfigCreateResponse> {
-    const { account_id, ...body } = params;
+    const { account_id = this._client.accountId, ...body } = params;
     return (
       this._client.post(`/accounts/${account_id}/ai-gateway/gateways/${gatewayId}/provider_configs`, {
         body,
@@ -54,10 +55,22 @@ export class ProviderConfigs extends APIResource {
    */
   list(
     gatewayId: string,
-    params: ProviderConfigListParams,
+    params?: ProviderConfigListParams,
+    options?: Core.RequestOptions,
+  ): Core.PagePromise<ProviderConfigListResponsesV4PagePaginationArray, ProviderConfigListResponse>;
+  list(
+    gatewayId: string,
+    options?: Core.RequestOptions,
+  ): Core.PagePromise<ProviderConfigListResponsesV4PagePaginationArray, ProviderConfigListResponse>;
+  list(
+    gatewayId: string,
+    params: ProviderConfigListParams | Core.RequestOptions = {},
     options?: Core.RequestOptions,
   ): Core.PagePromise<ProviderConfigListResponsesV4PagePaginationArray, ProviderConfigListResponse> {
-    const { account_id, ...query } = params;
+    if (isRequestOptions(params)) {
+      return this.list(gatewayId, {}, params);
+    }
+    const { account_id = this._client.accountId, ...query } = params;
     return this._client.getAPIList(
       `/accounts/${account_id}/ai-gateway/gateways/${gatewayId}/provider_configs`,
       ProviderConfigListResponsesV4PagePaginationArray,
@@ -122,7 +135,7 @@ export interface ProviderConfigCreateParams {
   /**
    * Path param
    */
-  account_id: string;
+  account_id?: string;
 
   /**
    * Body param
@@ -164,7 +177,7 @@ export interface ProviderConfigListParams extends V4PagePaginationArrayParams {
   /**
    * Path param
    */
-  account_id: string;
+  account_id?: string;
 }
 
 ProviderConfigs.ProviderConfigListResponsesV4PagePaginationArray =
