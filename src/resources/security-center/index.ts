84cafc13040e45ad55343885380269567c0c002b
Musa Jundi <musa@cloudflare.com>
2026-04-22 16:30:10 -0500

    feat(security-center): update generated types and methods

--- src/resources/security-center/index.ts
+++ src/resources/security-center/index.ts
@@ -1,4 +1,11 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-export { Insights } from './insights/index';
+export {
+  InsightListResponsesV4PagePagination,
+  Insights,
+  type InsightListResponse,
+  type InsightDismissResponse,
+  type InsightListParams,
+  type InsightDismissParams,
+} from './insights/index';
 export { SecurityCenter } from './security-center';
