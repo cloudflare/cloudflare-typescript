8fbcbc60268b9d791deb00a8c494a48b9e6d54fe
Musa Jundi <musa@cloudflare.com>
2026-04-22 16:30:07 -0500

    feat(managed-transforms): update generated types and methods

--- src/resources/managed-transforms/index.ts
+++ src/resources/managed-transforms/index.ts
@@ -1,3 +1,3 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-export * from './managed-transforms/index';
+export { ManagedTransforms } from './managed-transforms';
