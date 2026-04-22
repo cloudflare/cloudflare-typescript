f3b03d2d993cce5d3a6617e3b30c78141db342c2
Musa Jundi <musa@cloudflare.com>
2026-04-22 16:30:05 -0500

    feat(keyless-certificates): update generated types and methods

--- src/resources/keyless-certificates/index.ts
+++ src/resources/keyless-certificates/index.ts
@@ -1,3 +1,3 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-export * from './keyless-certificates/index';
+export { KeylessCertificates } from './keyless-certificates';
