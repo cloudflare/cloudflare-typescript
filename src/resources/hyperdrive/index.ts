85584e94b77a3f71ea656b78796b795f080278fd
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-29 13:41:47 -0700

    chore(hyperdrive): rename HyperdriveResource back to Hyperdrive, remove deleted tests

--- src/resources/hyperdrive/index.ts
+++ src/resources/hyperdrive/index.ts
@@ -1,13 +1,4 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-export {
-  Configs,
-  type ConfigDeleteResponse,
-  type ConfigCreateParams,
-  type ConfigUpdateParams,
-  type ConfigListParams,
-  type ConfigDeleteParams,
-  type ConfigEditParams,
-  type ConfigGetParams,
-} from './configs';
-export { HyperdriveResource } from './hyperdrive';
+export { Configs } from './configs';
+export { Hyperdrive } from './hyperdrive';
