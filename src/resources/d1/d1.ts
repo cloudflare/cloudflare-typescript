160e4272f596fb08d3c441104ad6c746282bb0e8
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-29 13:41:41 -0700

    chore(d1): rename D1Resource back to D1, remove deleted tests

--- src/resources/d1/d1.ts
+++ src/resources/d1/d1.ts
@@ -2,115 +2,14 @@
 
 import { APIResource } from '../../resource';
 import * as DatabaseAPI from './database/database';
-import {
-  Database,
-  DatabaseCreateParams,
-  DatabaseDeleteParams,
-  DatabaseDeleteResponse,
-  DatabaseEditParams,
-  DatabaseExportParams,
-  DatabaseExportResponse,
-  DatabaseGetParams,
-  DatabaseImportParams,
-  DatabaseImportResponse,
-  DatabaseListParams,
-  DatabaseListResponse,
-  DatabaseListResponsesV4PagePaginationArray,
-  DatabaseQueryParams,
-  DatabaseRawParams,
-  DatabaseRawResponse,
-  DatabaseRawResponsesSinglePage,
-  DatabaseUpdateParams,
-  QueryResult,
-  QueryResultsSinglePage,
-} from './database/database';
+import { Database } from './database/database';
 
-export class D1Resource extends APIResource {
+export class D1 extends APIResource {
   database: DatabaseAPI.Database = new DatabaseAPI.Database(this._client);
 }
 
-/**
- * The details of the D1 database.
- */
-export interface D1 {
-  /**
-   * Specifies the timestamp the resource was created as an ISO8601 string.
-   */
-  created_at?: string;
+D1.Database = Database;
 
-  /**
-   * The D1 database's size, in bytes.
-   */
-  file_size?: number;
-
-  /**
-   * Specify the location to restrict the D1 database to run and store data. If this
-   * option is present, the location hint is ignored.
-   */
-  jurisdiction?: 'eu' | 'fedramp' | null;
-
-  /**
-   * D1 database name.
-   */
-  name?: string;
-
-  num_tables?: number;
-
-  /**
-   * Configuration for D1 read replication.
-   */
-  read_replication?: D1.ReadReplication;
-
-  /**
-   * D1 database identifier (UUID).
-   */
-  uuid?: string;
-
-  version?: string;
-}
-
-export namespace D1 {
-  /**
-   * Configuration for D1 read replication.
-   */
-  export interface ReadReplication {
-    /**
-     * The read replication mode for the database. Use 'auto' to create replicas and
-     * allow D1 automatically place them around the world, or 'disabled' to not use any
-     * database replicas (it can take a few hours for all replicas to be deleted).
-     */
-    mode: 'auto' | 'disabled';
-  }
-}
-
-D1Resource.Database = Database;
-D1Resource.DatabaseListResponsesV4PagePaginationArray = DatabaseListResponsesV4PagePaginationArray;
-D1Resource.QueryResultsSinglePage = QueryResultsSinglePage;
-D1Resource.DatabaseRawResponsesSinglePage = DatabaseRawResponsesSinglePage;
-
-export declare namespace D1Resource {
-  export { type D1 as D1 };
-
-  export {
-    Database as Database,
-    type QueryResult as QueryResult,
-    type DatabaseListResponse as DatabaseListResponse,
-    type DatabaseDeleteResponse as DatabaseDeleteResponse,
-    type DatabaseExportResponse as DatabaseExportResponse,
-    type DatabaseImportResponse as DatabaseImportResponse,
-    type DatabaseRawResponse as DatabaseRawResponse,
-    DatabaseListResponsesV4PagePaginationArray as DatabaseListResponsesV4PagePaginationArray,
-    QueryResultsSinglePage as QueryResultsSinglePage,
-    DatabaseRawResponsesSinglePage as DatabaseRawResponsesSinglePage,
-    type DatabaseCreateParams as DatabaseCreateParams,
-    type DatabaseUpdateParams as DatabaseUpdateParams,
-    type DatabaseListParams as DatabaseListParams,
-    type DatabaseDeleteParams as DatabaseDeleteParams,
-    type DatabaseEditParams as DatabaseEditParams,
-    type DatabaseExportParams as DatabaseExportParams,
-    type DatabaseGetParams as DatabaseGetParams,
-    type DatabaseImportParams as DatabaseImportParams,
-    type DatabaseQueryParams as DatabaseQueryParams,
-    type DatabaseRawParams as DatabaseRawParams,
-  };
+export declare namespace D1 {
+  export { Database as Database };
 }
--- src/resources/d1/index.ts
+++ src/resources/d1/index.ts
@@ -1,25 +1,4 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-export { D1Resource } from './d1';
-export {
-  DatabaseListResponsesV4PagePaginationArray,
-  QueryResultsSinglePage,
-  DatabaseRawResponsesSinglePage,
-  Database,
-  type QueryResult,
-  type DatabaseListResponse,
-  type DatabaseDeleteResponse,
-  type DatabaseExportResponse,
-  type DatabaseImportResponse,
-  type DatabaseRawResponse,
-  type DatabaseCreateParams,
-  type DatabaseUpdateParams,
-  type DatabaseListParams,
-  type DatabaseDeleteParams,
-  type DatabaseEditParams,
-  type DatabaseExportParams,
-  type DatabaseGetParams,
-  type DatabaseImportParams,
-  type DatabaseQueryParams,
-  type DatabaseRawParams,
-} from './database/index';
+export { D1 } from './d1';
+export { Database } from './database/index';
