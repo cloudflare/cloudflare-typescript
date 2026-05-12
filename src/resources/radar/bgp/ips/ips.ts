9af4cdefbf1f5f26a509bd43cb192aa77e25f830
Vaishak Dinesh <vaishak@cloudflare.com>
2026-05-12 16:44:34 -0700

    feat(radar): add bgp/ips/index, bgp/ips/ips, bgp/ips/top

---
+++ src/resources/radar/bgp/ips/ips.ts
@@ -0,0 +1,295 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+import { APIResource } from '../../../../resource';
+import { isRequestOptions } from '../../../../core';
+import * as Core from '../../../../core';
+import * as TopAPI from './top';
+import { Top, TopAsesParams, TopAsesResponse } from './top';
+
+export class IPs extends APIResource {
+  top: TopAPI.Top = new TopAPI.Top(this._client);
+
+  /**
+   * Retrieves time series data for the announced IP space count, represented as the
+   * number of IPv4 /24s and IPv6 /48s, for a given ASN.
+   *
+   * @example
+   * ```ts
+   * const response = await client.radar.bgp.ips.timeseries();
+   * ```
+   */
+  timeseries(
+    query?: IPTimeseriesParams,
+    options?: Core.RequestOptions,
+  ): Core.APIPromise<IPTimeseriesResponse>;
+  timeseries(options?: Core.RequestOptions): Core.APIPromise<IPTimeseriesResponse>;
+  timeseries(
+    query: IPTimeseriesParams | Core.RequestOptions = {},
+    options?: Core.RequestOptions,
+  ): Core.APIPromise<IPTimeseriesResponse> {
+    if (isRequestOptions(query)) {
+      return this.timeseries({}, query);
+    }
+    return (
+      this._client.get('/radar/bgp/ips/timeseries', { query, ...options }) as Core.APIPromise<{
+        result: IPTimeseriesResponse;
+      }>
+    )._thenUnwrap((obj) => obj.result);
+  }
+}
+
+export interface IPTimeseriesResponse {
+  /**
+   * Metadata for the results.
+   */
+  meta: IPTimeseriesResponse.Meta;
+
+  serie_0: IPTimeseriesResponse.Serie0;
+}
+
+export namespace IPTimeseriesResponse {
+  /**
+   * Metadata for the results.
+   */
+  export interface Meta {
+    /**
+     * Aggregation interval of the results (e.g., in 15 minutes or 1 hour intervals).
+     * Refer to
+     * [Aggregation intervals](https://developers.cloudflare.com/radar/concepts/aggregation-intervals/).
+     */
+    aggInterval: 'FIFTEEN_MINUTES' | 'ONE_HOUR' | 'ONE_DAY' | 'ONE_WEEK' | 'ONE_MONTH';
+
+    confidenceInfo: Meta.ConfidenceInfo;
+
+    dateRange: Array<Meta.DateRange>;
+
+    /**
+     * Timestamp of the last dataset update.
+     */
+    lastUpdated: string;
+
+    /**
+     * Normalization method applied to the results. Refer to
+     * [Normalization methods](https://developers.cloudflare.com/radar/concepts/normalization/).
+     */
+    normalization:
+      | 'PERCENTAGE'
+      | 'MIN0_MAX'
+      | 'MIN_MAX'
+      | 'RAW_VALUES'
+      | 'PERCENTAGE_CHANGE'
+      | 'ROLLING_AVERAGE'
+      | 'OVERLAPPED_PERCENTAGE'
+      | 'RATIO';
+
+    /**
+     * Measurement units for the results.
+     */
+    units: Array<Meta.Unit>;
+
+    delay?: Meta.Delay;
+  }
+
+  export namespace Meta {
+    export interface ConfidenceInfo {
+      annotations: Array<ConfidenceInfo.Annotation>;
+
+      /**
+       * Provides an indication of how much confidence Cloudflare has in the data.
+       */
+      level: number;
+    }
+
+    export namespace ConfidenceInfo {
+      /**
+       * Annotation associated with the result (e.g. outage or other type of event).
+       */
+      export interface Annotation {
+        /**
+         * Data source for annotations.
+         */
+        dataSource:
+          | 'ALL'
+          | 'AI_BOTS'
+          | 'AI_GATEWAY'
+          | 'BGP'
+          | 'BOTS'
+          | 'CONNECTION_ANOMALY'
+          | 'CT'
+          | 'DNS'
+          | 'DNS_MAGNITUDE'
+          | 'DNS_AS112'
+          | 'DOS'
+          | 'EMAIL_ROUTING'
+          | 'EMAIL_SECURITY'
+          | 'FW'
+          | 'FW_PG'
+          | 'HTTP'
+          | 'HTTP_CONTROL'
+          | 'HTTP_CRAWLER_REFERER'
+          | 'HTTP_ORIGINS'
+          | 'IQI'
+          | 'LEAKED_CREDENTIALS'
+          | 'NET'
+          | 'ROBOTS_TXT'
+          | 'SPEED'
+          | 'WORKERS_AI';
+
+        description: string;
+
+        endDate: string;
+
+        /**
+         * Event type for annotations.
+         */
+        eventType: 'EVENT' | 'GENERAL' | 'OUTAGE' | 'PARTIAL_PROJECTION' | 'PIPELINE' | 'TRAFFIC_ANOMALY';
+
+        /**
+         * Whether event is a single point in time or a time range.
+         */
+        isInstantaneous: boolean;
+
+        linkedUrl: string;
+
+        startDate: string;
+      }
+    }
+
+    export interface DateRange {
+      /**
+       * Adjusted end of date range.
+       */
+      endTime: string;
+
+      /**
+       * Adjusted start of date range.
+       */
+      startTime: string;
+    }
+
+    export interface Unit {
+      name: string;
+
+      value: string;
+    }
+
+    export interface Delay {
+      asn_data: Delay.ASNData;
+
+      country_data: Delay.CountryData;
+
+      healthy: boolean;
+
+      nowTs: number;
+    }
+
+    export namespace Delay {
+      export interface ASNData {
+        delaySecs: number;
+
+        delayStr: string;
+
+        healthy: boolean;
+
+        latest: ASNData.Latest;
+      }
+
+      export namespace ASNData {
+        export interface Latest {
+          entries_count: number;
+
+          path: string;
+
+          timestamp: number;
+        }
+      }
+
+      export interface CountryData {
+        delaySecs: number;
+
+        delayStr: string;
+
+        healthy: boolean;
+
+        latest: CountryData.Latest;
+      }
+
+      export namespace CountryData {
+        export interface Latest {
+          count: number;
+
+          timestamp: number;
+        }
+      }
+    }
+  }
+
+  export interface Serie0 {
+    ipv4: Array<string>;
+
+    ipv6: Array<string>;
+
+    timestamps: Array<string>;
+  }
+}
+
+export interface IPTimeseriesParams {
+  /**
+   * Filters results by Autonomous System. Specify one or more Autonomous System
+   * Numbers (ASNs) as a comma-separated list. Prefix with `-` to exclude ASNs from
+   * results. For example, `-174, 3356` excludes results from AS174, but includes
+   * results from AS3356.
+   */
+  asn?: Array<string>;
+
+  /**
+   * End of the date range (inclusive).
+   */
+  dateEnd?: Array<string>;
+
+  /**
+   * Filters results by date range. For example, use `7d` and `7dcontrol` to compare
+   * this week with the previous week. Use this parameter or set specific start and
+   * end dates (`dateStart` and `dateEnd` parameters).
+   */
+  dateRange?: Array<string>;
+
+  /**
+   * Start of the date range.
+   */
+  dateStart?: Array<string>;
+
+  /**
+   * Format in which results will be returned.
+   */
+  format?: 'JSON' | 'CSV';
+
+  /**
+   * Includes data delay meta information.
+   */
+  includeDelay?: boolean;
+
+  /**
+   * Filters results by IP version (Ipv4 vs. IPv6).
+   */
+  ipVersion?: Array<'IPv4' | 'IPv6'>;
+
+  /**
+   * Filters results by location. Specify a comma-separated list of alpha-2 location
+   * codes.
+   */
+  location?: Array<string>;
+
+  /**
+   * Array of names used to label the series in the response.
+   */
+  name?: Array<string>;
+}
+
+IPs.Top = Top;
+
+export declare namespace IPs {
+  export { type IPTimeseriesResponse as IPTimeseriesResponse, type IPTimeseriesParams as IPTimeseriesParams };
+
+  export { Top as Top, type TopAsesResponse as TopAsesResponse, type TopAsesParams as TopAsesParams };
+}
---
+++ tests/api-resources/radar/bgp/ips/ips.test.ts
@@ -0,0 +1,50 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+import Cloudflare from 'cloudflare';
+import { Response } from 'node-fetch';
+
+const client = new Cloudflare({
+  apiKey: '144c9defac04969c7bfad8efaa8ea194',
+  apiEmail: 'user@example.com',
+  baseURL: process.env['TEST_API_BASE_URL'] ?? 'http://127.0.0.1:4010',
+});
+
+describe('resource ips', () => {
+  test('timeseries', async () => {
+    const responsePromise = client.radar.bgp.ips.timeseries();
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  test('timeseries: request options instead of params are passed correctly', async () => {
+    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
+    await expect(client.radar.bgp.ips.timeseries({ path: '/_stainless_unknown_path' })).rejects.toThrow(
+      Cloudflare.NotFoundError,
+    );
+  });
+
+  test('timeseries: request options and params are passed correctly', async () => {
+    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
+    await expect(
+      client.radar.bgp.ips.timeseries(
+        {
+          asn: ['string'],
+          dateEnd: ['2019-12-27T18:11:19.117Z'],
+          dateRange: ['7d'],
+          dateStart: ['2019-12-27T18:11:19.117Z'],
+          format: 'JSON',
+          includeDelay: true,
+          ipVersion: ['IPv4'],
+          location: ['string'],
+          name: ['main_series'],
+        },
+        { path: '/_stainless_unknown_path' },
+      ),
+    ).rejects.toThrow(Cloudflare.NotFoundError);
+  });
+});
