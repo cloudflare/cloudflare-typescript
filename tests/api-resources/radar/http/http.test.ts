6446c29f7935d76025ef67e8922d06a92ce96d4c
Vaishak Dinesh <vaishak@cloudflare.com>
2026-05-20 09:22:34 -0700

    chore(radar): update codegen output

--- tests/api-resources/radar/http/http.test.ts
+++ tests/api-resources/radar/http/http.test.ts
@@ -87,6 +87,7 @@ describe('resource http', () => {
           asn: ['string'],
           botClass: ['LIKELY_AUTOMATED'],
           browserFamily: ['CHROME'],
+          contentType: ['HTML'],
           continent: ['string'],
           dateEnd: ['2019-12-27T18:11:19.117Z'],
           dateRange: ['7d'],
