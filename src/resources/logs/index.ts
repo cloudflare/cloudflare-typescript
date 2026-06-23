4b79b45af2e22009161088028f32b89411fc3b4e
Musa Jundi <musa@cloudflare.com>
2026-06-23 12:25:47 -0500

    feat(logs): add log-explorer sub-resource

--- src/resources/logs/index.ts
+++ src/resources/logs/index.ts
@@ -1,6 +1,7 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 export { Control } from './control/index';
+export { LogExplorer } from './log-explorer/index';
 export { Logs } from './logs';
 export { RayID, type RayIDGetResponse, type RayIDGetParams } from './rayid';
 export { Received, type ReceivedGetResponse, type ReceivedGetParams } from './received/index';
---
+++ tests/api-resources/logs/log-explorer/datasets/datasets.test.ts
@@ -0,0 +1,78 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+import Cloudflare from 'cloudflare';
+import { Response } from 'node-fetch';
+
+const client = new Cloudflare({
+  apiKey: '144c9defac04969c7bfad8efaa8ea194',
+  apiEmail: 'user@example.com',
+  baseURL: process.env['TEST_API_BASE_URL'] ?? 'http://127.0.0.1:4010',
+});
+
+describe('resource datasets', () => {
+  test('create: only required params', async () => {
+    const responsePromise = client.logs.logExplorer.datasets.create({
+      dataset: 'dataset',
+      account_id: 'account_id',
+    });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  test('create: required and optional params', async () => {
+    const response = await client.logs.logExplorer.datasets.create({
+      dataset: 'dataset',
+      account_id: 'account_id',
+      fields: [{ enabled: true, name: 'name' }],
+    });
+  });
+
+  test('update: only required params', async () => {
+    const responsePromise = client.logs.logExplorer.datasets.update('dataset_id', {
+      enabled: true,
+      account_id: 'account_id',
+    });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  test('update: required and optional params', async () => {
+    const response = await client.logs.logExplorer.datasets.update('dataset_id', {
+      enabled: true,
+      account_id: 'account_id',
+      fields: [{ enabled: true, name: 'name' }],
+    });
+  });
+
+  test('list', async () => {
+    const responsePromise = client.logs.logExplorer.datasets.list({ account_id: 'account_id' });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  test('get', async () => {
+    const responsePromise = client.logs.logExplorer.datasets.get('dataset_id', { account_id: 'account_id' });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+});
