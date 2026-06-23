f424bee5791d76683534ba0721a1e79cba1c1954
Musa Jundi <musa@cloudflare.com>
2026-06-23 12:25:46 -0500

    feat(email-auth): add EmailAuth resource

---
+++ src/resources/email-auth/index.ts
@@ -0,0 +1,11 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+export {
+  DMARCReports,
+  type DMARCReportEditResponse,
+  type DMARCReportGetResponse,
+  type DMARCReportEditParams,
+  type DMARCReportGetParams,
+} from './dmarc-reports';
+export { EmailAuth } from './email-auth';
+export { SPF } from './spf/index';
