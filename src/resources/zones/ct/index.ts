9571feaa818ea9dce589311de6e626458d42d11f
Musa Jundi <musa@cloudflare.com>
2026-06-23 12:25:49 -0500

    feat(zones): add CT alerting sub-resource

---
+++ src/resources/zones/ct/index.ts
@@ -0,0 +1,10 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+export {
+  Alerting,
+  type AlertingEditResponse,
+  type AlertingGetResponse,
+  type AlertingEditParams,
+  type AlertingGetParams,
+} from './alerting';
+export { CT } from './ct';
