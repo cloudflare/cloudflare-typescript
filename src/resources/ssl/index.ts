667b5b3cda91552db9614fe9d4af2e9442028d25
Musa Jundi <musa@cloudflare.com>
2026-06-23 12:25:48 -0500

    chore(ssl): update codegen output

--- src/resources/ssl/index.ts
+++ src/resources/ssl/index.ts
@@ -1,7 +1,13 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 export { Analyze, type AnalyzeCreateResponse, type AnalyzeCreateParams } from './analyze';
-export { AutoOriginTLSKex } from './auto-origin-tls-kex';
+export {
+  AutoOriginTLSKex,
+  type AutoOriginTLSKexEditResponse,
+  type AutoOriginTLSKexGetResponse,
+  type AutoOriginTLSKexEditParams,
+  type AutoOriginTLSKexGetParams,
+} from './auto-origin-tls-kex';
 export {
   CertificatePackListResponsesV4PagePaginationArray,
   CertificatePacks,
@@ -20,11 +26,7 @@ export {
   type CertificatePackEditParams,
   type CertificatePackGetParams,
 } from './certificate-packs/index';
-export {
-  Recommendations,
-  type RecommendationGetResponse,
-  type RecommendationGetParams,
-} from './recommendations';
+export { Recommendations } from './recommendations';
 export { SSL } from './ssl';
 export { Universal } from './universal/index';
 export {
