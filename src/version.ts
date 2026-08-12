43da2d79427f576045668b439a48e6c4f84b6beb
rotem <rotem@cloudflare.com>
2026-08-12 14:57:11 -0500

    chore: update release-please version to generated

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = '7.0.0'; // x-release-please-version
+export const VERSION = '0.0.1'; // x-release-please-version
 
 // Set by codegen; empty string omits the header.
 export const API_VERSION = '';
